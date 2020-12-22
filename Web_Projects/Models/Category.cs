a206b3f3f3aa3e009796224b315ac1fe34eccfee
Burak Can <57682748+BcanGRG@users.noreply.github.com>
2020-12-23 02:03:51 +0300

    Add project files.

---
+++ Web_Projects/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Projects.Models
+{
+    public class Category
+    {
+        public int CategoryID { get; set; }
+    }
+}
